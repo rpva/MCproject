9cc8e53a70f51a816419070f67ecb6e474480a64
RuiAlves <59886567+rpva@users.noreply.github.com>
2020-03-24 16:20:25 +0000

    First commit, to initialize repository

---
+++ app/src/main/java/pt/ulisboa/tecnico/sise/mc/project/insureappgroup10/MainActivity.java
@@ -0,0 +1,14 @@
+package pt.ulisboa.tecnico.sise.mc.project.insureappgroup10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
